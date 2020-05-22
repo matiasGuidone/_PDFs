48f98ba7436f4b91a36e46dc0bf11ca1c4c4bdfa
matiasGuidone <matias.guidone@gmail.com>
2020-05-21 21:22:25 -0300

    m

--- index.php
+++ index.php
@@ -9,8 +9,9 @@
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="./css/main.css">
 	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
+
 		<style>
 		.canvas-cont {
  			height: 100%;
